0ff65403619d390c69c752d29405e34fc75ae61e
1shyamp9 <1shyamp9@gmail.com>
2023-07-24 20:39:09 +0530

    Cors 4

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ app.use(express.json());
 app.use(cookieParser())
 // app.use(cors({
 //     origin:[process.env.FRONTEND_URL],
-//     methods:['POST','GET','PUT','DELETE'],
-//     credential:true
+//     methods:["GET","POST","PUT","DELETE"],
+//     credentials:true,
 // }))
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true,
+}))
 app.use('/user',userRouter)
 
 configDotenv({ path: './database/config.env' });
-
-app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send("Nice Api is Working...")
